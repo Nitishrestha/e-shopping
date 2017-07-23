1b66e4f98698680f1695848ff50fc3264c96a552
Nitish Shrestha <nitishrestha8848@gmail.com>
2017-07-23 11:31:32 +0545

    completed the interface theme and other issues were solved...

--- onlineshopping/src/main/java/net/nitish/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/nitish/onlineshopping/controller/PageController.java
@@ -5,14 +5,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class PageController
-{
-	@RequestMapping(value = {"/","/home","/index"})
-	public ModelAndView index()
-	{
+public class PageController {
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("page");
-		mav.addObject("greeting","Hello  World of Spring");
-		
+		mav.addObject("title", "Home");
+		mav.addObject("userClickHome", true);
+
+		return mav;
+	}
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("title", "About Us");
+		mav.addObject("userClickAbout", true);
+
+		return mav;
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("title", "Contact Us");
+		mav.addObject("userClickContact", true);
+
 		return mav;
 	}
 
